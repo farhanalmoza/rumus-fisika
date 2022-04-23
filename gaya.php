f2bf41ee2a59641468c5dfca1cb737fae3ce9fca
Farhan <oyisamfarhan@gmail.com>
2022-04-23 07:33:09 +0700

    sidebar

--- gaya.php
+++ gaya.php
@@ -1,76 +1,105 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Gaya | Kelompok 2</title>
-  </head>
-  <body>
-		<div class="container">	
-
-			<!-- Card Form Rumus -->
-			<div class="card mt-5">
-				<div class="card-header">Rumus Gaya</div>
-				<div class="card-body">
-					<form>
-						<div class="mb-3">
-							<div class="row g-3 align-items-center">
-								<div class="col-md-2">
-									<label for="massa" class="col-form-label">Massa Benda</label>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Gaya | Kelompok 2</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="d-flex" id="wrapper">
+            <!-- Sidebar-->
+            <div class="border-end bg-white" id="sidebar-wrapper">
+                <div class="sidebar-heading border-bottom bg-light">Rumus Fisika</div>
+                <div class="list-group list-group-flush">
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="./e_potensial.php">Energi Potensial</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3 active" href="./gaya.php">Gaya</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="./massa_jenis.php">Massa Jenis</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="./panjang_gelombang.php">Panjang Gelombang</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="./percepatan.php">Percepatan</a>
+                </div>
+            </div>
+            <!-- Page content wrapper-->
+            <div id="page-content-wrapper">
+                <!-- Top navigation-->
+                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                    <div class="container-fluid">
+                        <button class="btn btn-primary" id="sidebarToggle">Toggle Menu</button>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                                <li class="nav-item active"><a class="nav-link" href="#!">Kelompok 2</a></li>
+                        </div>
+                    </div>
+                </nav>
+                <!-- Page content-->
+                <div class="container-fluid">
+                    <!-- Card Form Rumus -->
+					<div class="card mt-4">
+						<div class="card-header">Rumus Gaya</div>
+						<div class="card-body">
+							<form>
+								<div class="mb-3">
+									<div class="row g-3 align-items-center">
+										<div class="col-md-2">
+											<label for="massa" class="col-form-label">Massa Benda</label>
+										</div>
+										<div class="col-md-6">
+											<input type="number" id="massa" class="form-control" aria-describedby="massanHelpInline">
+										</div>
+										<div class="col-md-auto">
+											<span id="massanHelpInline" class="form-text">
+												dalam satuan kilogram (kg)
+											</span>
+										</div>
+									</div>
 								</div>
-								<div class="col-md-6">
-									<input type="number" id="massa" class="form-control" aria-describedby="massanHelpInline">
+								<div class="mb-3">
+									<div class="row g-3 align-items-center">
+										<div class="col-md-2">
+											<label for="percepatan" class="col-form-label">Percepatan</label>
+										</div>
+										<div class="col-md-6">
+											<input type="number" id="percepatan" class="form-control" aria-describedby="percepatanHelpInline">
+										</div>
+										<div class="col-md-auto">
+											<span id="percepatanHelpInline" class="form-text">
+												(m/s^2)
+											</span>
+										</div>
+									</div>
 								</div>
-								<div class="col-md-auto">
-									<span id="massanHelpInline" class="form-text">
-										dalam satuan kilogram (kg)
-									</span>
-								</div>
-							</div>
+
+								<button type="button" id="submitGaya" class="btn btn-primary">Hitung</button>
+							</form>
 						</div>
-						<div class="mb-3">
-							<div class="row g-3 align-items-center">
-								<div class="col-md-2">
-									<label for="percepatan" class="col-form-label">Percepatan</label>
-								</div>
-								<div class="col-md-6">
-									<input type="number" id="percepatan" class="form-control" aria-describedby="percepatanHelpInline">
-								</div>
-								<div class="col-md-auto">
-									<span id="percepatanHelpInline" class="form-text">
-										(m/s^2)
-									</span>
-								</div>
-							</div>
+					</div>
+					<!-- End Card Form Rumus -->
+
+					<!-- Card Hasil -->
+					<div class="card mt-3">
+						<div class="card-header">Hasil Perhitungan Gaya</div>
+						<div class="card-body">
+							<h5 class="card-title" id="hasilGaya"></h5>
 						</div>
+					</div>
+					<!-- End Card Hasil -->
+                </div>
+            </div>
+        </div>
 
-						<button type="button" id="submitGaya" class="btn btn-primary">Hitung</button>
-					</form>
-				</div>
-			</div>
-			<!-- End Card Form Rumus -->
-
-			<!-- Card Hasil -->
-			<div class="card mt-3">
-				<div class="card-header">Hasil Perhitungan Gaya</div>
-				<div class="card-body">
-					<h5 class="card-title" id="hasilGaya"></h5>
-				</div>
-			</div>
-			<!-- End Card Hasil -->
-
-		</div>
-
-    <!-- Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 		<!-- jQuery -->
 		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-		<!-- My Script -->
-		<script src="./js/script.js"></script>
-  </body>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="./js/script.js"></script>
+		<!-- sidebar -->
+		<script src="./js/sidebar.js"></script>
+    </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// sidebar
+window.addEventListener('DOMContentLoaded', event => {
+    // Toggle the side navigation
+    const sidebarToggle = $('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+});
+
 // Hasil untuk rumus percepatan
 $('#submitPercepatan').click(function() {
     let kecepatan = $('#kecepatan').val();
