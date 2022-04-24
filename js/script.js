48f0888d01080c0e178cdbd1f6f89e2c7fa210a3
faris-studio <farissyaifulloh123@gmail.com>
2022-04-24 14:29:38 +0700

    periode123

--- js/script.js
+++ js/script.js
@@ -71,3 +71,26 @@ $('#submitrangkaianlistrikohm').click(function() {
     let rangkaianlistrikohm  = kuatarus * hambatan;
     $('#hasilrangkaianlistrikohm').html(`Hasil rangkaian listriknya adalah ${rangkaianlistrikohm} V`);
 })
+
+// Hasil untuk rumus periode1
+$('#submitPeriode1').click(function() {
+    let waktu = $('#waktu').val();
+    let getaran = $('#getaran').val();
+    let Periode1  = waktu / getaran;
+    $('#hasilPeriode1').html(`Hasil Periode1 adalah ${Periode1} S`);
+})
+
+// Hasil untuk rumus periode2
+$('#submitPeriode1').click(function() {
+    let frekuensi = $('#getaran').val();
+    let Periode2  = 1 / frekuensi;
+    $('#hasilPeriode2').html(`Hasil Periode2 adalah ${Periode2} S`);
+})
+
+// Hasil untuk rumus periode3
+$('#submitPeriode3').click(function() {
+    let kecepatangelombang = $('#kecepatangelombang').val();
+    let panjanggelombang = $('#panjanggelombang').val();
+    let Periode3  = kecepatangelombang * panjanggelombang;
+    $('#hasilPeriode3').html(`Hasil Periode3 adalah ${Periode3} S`);
+})
